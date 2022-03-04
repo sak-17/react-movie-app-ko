c2f672faef587f3518f3a54ee7229b282886c113
sak-17 <seakwon17@gmail.com>
2022-03-04 14:06:44 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'http://api.themoviedb.org/3/';
+export const API_KEY = '1e626d8cc4e6d413112d44543788ef6b';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
